e2aa8c4cbb074a964375c8e8b37da74c66a07264
Linda <lb55657@fer.hr>
2026-01-29 15:25:07 +0100

    Delete hardware/plot/control directory

--- hardware/plot/control/pid_controller.py
+++
@@ -1,7 +0,0 @@
-from simple_pid import PID
-from config.config import KP, KI, KD, SOIL_SETPOINT, PID_MIN, PID_MAX
-
-def create_pid():
-    pid = PID(KP, KI, KD, setpoint=SOIL_SETPOINT)
-    pid.output_limits = (PID_MIN, PID_MAX)
-    return pid
